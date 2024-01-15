526be0b99f9ac736b392137331d42816306a2be9
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2024-01-15 00:30:33 -0500

    Modified api calls and added Spinner

--- components/productForm.js
+++ components/productForm.js
@@ -2,17 +2,20 @@ import Layout from "@/components/layout";
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import Spinner from "./spinner";
 
 export default function ProductForm({
     _id,
     title: existingTitle,
     description: existingDescription,
     price: existingPrice,
-    images,
+    images: existingImages,
 }) {
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
+    const [images, setImages] = useState(existingImages || []);
+    const [isUploading, setIsUploading] = useState(false);
     const [goToProducts, setGoToProducts] = useState(false);
     const router = useRouter();
     // console.log({ _id });
@@ -20,7 +23,7 @@ export default function ProductForm({
     // Most functions dealing with HTTP requests/responses are async
     async function saveProduct(ev) {
         ev.preventDefault();
-        const data = { title, description, price };
+        const data = { title, description, price, images };
         if (_id) {
             //update
             await axios.put('/api/products', { ...data, _id });
@@ -39,6 +42,7 @@ export default function ProductForm({
         // console.log(ev);
         const files = ev.target?.files;
         if (files?.length > 0) {
+            setIsUploading(true);
             const data = new FormData();
             for (const file of files) {
                 data.append('file', file);
@@ -46,11 +50,11 @@ export default function ProductForm({
             // const res = await axios.post('/api/upload', data, {
             //     headers: { 'Content-Type': 'multipart/form/data' },
             // });
-            const res = await fetch('/api/upload', {
-                method: 'POST',
-                body: data,
-            })
-            console.log(res);
+            const res = await axios.post('/api/upload', data);
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links];
+            });
+            setIsUploading(false);
         }
     }
     return (
@@ -64,7 +68,18 @@ export default function ProductForm({
             <label>
                 Photos
             </label>
-            <div className="mb-2">
+            <div className="mb-2 flex flex-wrap gap-2">
+                {!!images?.length && images.map(link => (
+                    <div key={link} class="h-24">
+                        <img src={link} alt="" className="rounded-lg" />
+                    </div>
+                ))}
+                {isUploading && (
+                    <div className="h-24 p1 flex items-center">
+                        {/* Uploading... */}
+                        <Spinner />
+                    </div>
+                )}
                 <label className="w-24 h-24 border cursor-pointer text-center flex flex-col items-center justify-center text-sm text-gray-600 rounded-md bg-gray-200">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
