f560c7e7e2fa241dc2a49c4ecaf01b6ef8eb14f6
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2024-01-17 23:56:31 -0500

    Update categories.js

--- pages/categories.js
+++ pages/categories.js
@@ -1,16 +1,26 @@
 import Layout from "@/components/layout";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Categories() {
-    function saveCategory() {
-
+    // Every Input needs a useState Hook to update the value
+    const [name, setName] = useState('');
+    async function saveCategory() {
+        await axios.post('/api/categories', { name });
+        setName('');
     }
     return (
         <Layout>
             <h1>Categories</h1>
             <label>New category name</label>
             <form onSubmit={saveCategory} className="flex gap-2">
-                <input className="mb-0" type="text" placeholder={'Category name'} />
-                <button type="submit" className="btn-primary">Save</button>
+                <input
+                    className="mb-0"
+                    type="text"
+                    placeholder={'Category name'}
+                    onChange={ev => setName(ev.target.value)}
+                    value={name} />
+                <button type="submit" className="btn-primary py-1">Save</button>
             </form>
         </Layout>
     );
