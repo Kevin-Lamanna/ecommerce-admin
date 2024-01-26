a352a338fc28b099be85c4a2a0534240d289374e
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2024-01-25 23:58:47 -0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,8 @@ import GoogleProvider from 'next-auth/providers/google'
 import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import clientPromise from '@/lib/mongodb'
 
+const adminEmails = ['kevin.lamanna2@gmail.com'];
+
 export default NextAuth({
     providers: [
         GoogleProvider({
@@ -11,13 +13,22 @@ export default NextAuth({
         }),
     ],
     adapter: MongoDBAdapter(clientPromise),
+    callbacks: {
+        session: ({ session, token, user }) => {
+            if (adminEmails.includes(session?.user?.email)) {
+                return session;
+            } else {
+                return false;
+            }
+        },
+    },
 })
 
-export async function isAdminRequest(req, res) {
-    const session = await getServerSession(req, res, authOptions);
-    if (!adminEmails.includes(session?.user?.email)) {
-        res.status(401);
-        res.end();
-        throw 'not an admin';
-    }
-}
+// export async function isAdminRequest(req, res) {
+//     const session = await getServerSession(req, res, authOptions);
+//     if (!adminEmails.includes(session?.user?.email)) {
+//         res.status(401);
+//         res.end();
+//         throw 'not an admin';
+//     }
+// }
