95803923392ac0b2cd1acf149183b127be007c45
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2024-01-26 15:41:04 -0500

    Update upload.js

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,6 +2,9 @@ import multiparty from 'multiparty';
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import fs from 'fs';
 import mime from 'mime-types';
+import { mongooseConnect } from "@/lib/mongoose";
+import { getServerSession } from "next-auth";
+import { authOptions, isAdminRequest } from "@/pages/api/auth/[...nextauth]";
 
 // Bucket name on AWS
 const bucketName = 'kevin-nextjs-ecommerce';
@@ -9,6 +12,7 @@ const bucketName = 'kevin-nextjs-ecommerce';
 // We install multiparty to deal with the form data
 // This function handles the upload request and response
 export default async function handle(req, res) {
+
     const form = new multiparty.Form();
     // grab the fields and files from the promise
     const { fields, files } = await new Promise((resolve, reject) => {
