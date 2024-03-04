5a2bff9f2de7999c811163568220a5cd2a821786
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2024-03-03 23:28:07 -0500

    Update orders.js

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,6 +1,7 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Order } from "@/models/order";
 
+// Handles GET requests and responses
 export default async function handler(req, res) {
     await mongooseConnect();
     res.json(await Order.find().sort({ createdAt: -1 }));
